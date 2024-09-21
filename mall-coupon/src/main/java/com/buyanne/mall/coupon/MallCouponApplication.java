303db296294034d18bd4f6a4b7ebd44e7f2e5c2b
王潇迪 <1903683577@qq.com>
2024-09-21 14:37:40 +0800

    增加nacos配置，同时增加gateway模块

--- mall-coupon/src/main/java/com/buyanne/mall/coupon/MallCouponApplication.java
+++ mall-coupon/src/main/java/com/buyanne/mall/coupon/MallCouponApplication.java
@@ -2,13 +2,16 @@ package com.buyanne.mall.coupon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MallCouponApplication {
 
 	public static void main(String[] args) {
+		System.setProperty("spring.cloud.bootstrap.enabled", "true");
 		SpringApplication.run(MallCouponApplication.class, args);
 	}
 
